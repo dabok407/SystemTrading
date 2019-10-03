2f6856ab09ab7a89d01b84e2fab9825fe7ffdf06
dabok407 <dabok407@naver.com>
2019-10-03 23:03:36 +0900

    dfsa

---
+++ Com/paintCellExcel.py
@@ -0,0 +1,9 @@
+import win32com.client
+
+excel = win32com.client.Dispatch("Excel.Application")
+excel.Visible = True
+wb = excel.Workbooks.Open('D:\\comTest\\readTest.xlsx')
+ws = wb.ActiveSheet
+ws.Cells(1,2).Value = "is"
+ws.Range("C1").Value = "good"
+ws.Range("C1").Interior.ColorIndex = 10
